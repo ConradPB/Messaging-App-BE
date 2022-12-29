47a70cb1554a69de09839ac898e298896b0e8d35
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2022-12-29 22:55:06 +0300

    project setup http methods

--- src/server.js
+++ src/server.js
@@ -1,13 +1,27 @@
 import 'dotenv/config'
-
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors())
+
 app.get('/', (req, res) => {
-    res.send('Here we go nodee uppp!');
+    res.send('Here we go nodee uppp! Received msg');
   })
 
-app.listen(3000, () =>
-  console.log('App is hot and listening on port 3000!'),
+app.post('/', (req, res) => {
+    res.send('Here we go nodee uppp! Created msg');
+  })
+
+app.put('/', (req, res) => {
+    res.send('Here we go nodee uppp! Updated msg');
+  })  
+
+app.delete('/', (req, res) => {
+    res.send('Here we go nodee uppp! dlt msg');
+  })
+  
+app.listen(process.env.PORT, () =>
+  console.log(`App is hot and listening on port ${process.env.PORT}!`),
 )
