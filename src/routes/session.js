82a9a2bf9f32c9f265e7469c23a61f48729d9c13
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-01-08 22:02:01 +0300

    added modular controller

--- src/routes/session.js
+++ src/routes/session.js
@@ -4,6 +4,8 @@ const router = Router()
 
 import Session from "../controllers/session"
 
+// *** create an instance of the controller class ***
+
 const session = new Session()
 
 router.get('/', session.getUser)
